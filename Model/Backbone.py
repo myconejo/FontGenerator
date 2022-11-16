8bd0be8dd4388e3a6f1277a3c9e58b909ded334e
Minyoung Choi/최민영 <96284664+myconejo@users.noreply.github.com>
2022-11-16 11:00:03 +0900

    Update Backbone.py

--- Model/Backbone.py
+++ Model/Backbone.py
@@ -16,8 +16,8 @@ class Generator(nn.Module):
 
 class Encoder(nn.Module):
     def __init__(self, input_dim=1, conv_dim=64):
-        self.conv1 = nn.Conv2d(input_dim, conv_dim, kernel_size=5, stride=2, padding=2,dilation=2)
-        self.conv2 = nn.Conv2d(conv_dim, conv_dim*2, kernel_size=5, stride=2, padding=2,dilation=2)
+        self.conv1 = nn.Conv2d(input_dim, conv_dim, kernel_size=5, stride=2, padding=2, dilation=2)
+        self.conv2 = nn.Conv2d(conv_dim, conv_dim*2, kernel_size=5, stride=2, padding=2, dilation=2)
         self.bn2 = nn.BatchNorm2d(conv_dim)
         self.lrelu = nn.LeakyReLU(0.2)
         
@@ -43,4 +43,44 @@ class Decoder(nn.Module):
         output2 = torch.cat((output2, encoder_dict['enc7']), dim=1)
         
         output = torch.tanh(output2)
-        return output       
+        return output   
+    
+class Discriminator(nn.Module):
+    def __init__(self, category_num, img_dim=3, disc_dim=64):
+        super(Discriminator, self).__init__()
+        
+        # Convolutional Layers
+        self.conv1 = nn.Conv2d(img_dim, disc_dim, kernel_size=3, stride=2, pad=1, dilation=1)
+        self.conv2 = nn.Conv2d(disc_dim, 2*disc_dim, kernel_size=3, stride=2, pad=1, dilation=1)
+        self.conv3 = nn.Conv2d(2*disc_dim, 4*disc_dim, kernel_size=3, stride=2, pad=1, dilation=1)
+        self.conv4 = nn.Conv2d(4*disc_dim, 8*disc_dim, kernel_size=3, stride=2, pad=1, dilation=1)
+        
+        # Fully Connected (Linear) Layers
+        self.fc1 = nn.Linear(512*disc_dim, 1)
+        self.fc2 = nn.Linear(512*disc_dim, category_num)
+        
+        # Supplementary Methods
+        self.lrelu = nn.LeakyReLU(0.2)
+        self.bnorm = nn.BatchNorm2d()
+        self.dropout = nn.Dropout()
+        
+    def forward(self, images):
+        # image: generated or sample data images for the discriminator
+        batch_size = images.shape[0]
+        output = self.conv1(images)
+        
+        output = self.conv2(output)
+        
+        output = self.conv3(output)
+        
+        output = self.conv4(output)
+        
+        output = output.reshape(batch_size, -1)
+        
+        tf_loss_logit = self.fc1(output)
+        tf_loss = torch.sigmoid(tf_loss_logit)
+        cat_loss = self.fc2(output)
+        
+        return (tf_loss, tf_loss_logit, cat_loss)
+        
+        
