c5ceecac73a842caa891290310b40be4a5d966d1
Minyoung Choi/최민영 <96284664+myconejo@users.noreply.github.com>
2022-11-16 11:09:20 +0900

    Update Backbone.py

--- Model/Backbone.py
+++ Model/Backbone.py
@@ -1,6 +1,31 @@
 import torch
 import torch.nn as nn
 
+###################################### HELPER FUNCTIONS ######################################
+# conv2d: nn.Conv2d with Leaky ReLU and Batch Normalization
+def conv2d(c_in, c_out, k_size=3, stride=2, pad=1, dilation=1, bn=True, lrelu=True, leak=0.2):
+    layers = []
+    if lrelu:
+        layers.append(nn.LeakyReLU(leak))
+    layers.append(nn.Conv2d(c_in, c_out, k_size, stride, pad))
+    if bn:
+        layers.append(nn.BatchNorm2d(c_out))
+    return nn.Sequential(*layers)
+
+# deconv2d: nn.Conv2d with Batch Normalization and Dropout
+def deconv2d(c_in, c_out, k_size=3, stride=1, pad=1, dilation=1, bn=True, dropout=False, p=0.5):
+    layers = []
+    layers.append(nn.LeakyReLU(0.2))
+    layers.append(nn.ConvTranspose2d(c_in, c_out, k_size, stride, pad))
+    if bn:
+        layers.append(nn.BatchNorm2d(c_out))
+    if dropout:
+        layers.append(nn.Dropout(p))
+    return nn.Sequential(*layers)
+
+###################################### HELPER FUNCTIONS ######################################
+
+# GENERATOR
 class Generator(nn.Module):
     def __init__(self, input_dim=1, conv_dim=64):
         super(Generator, self).__init__()
@@ -12,8 +37,7 @@ class Generator(nn.Module):
         decoder_result = self.decoder_model(embedded, encoder_dict)
         return decoder_result
         
-        
-
+# ENCODER
 class Encoder(nn.Module):
     def __init__(self, input_dim=1, conv_dim=64):
         self.conv1 = nn.Conv2d(input_dim, conv_dim, kernel_size=5, stride=2, padding=2, dilation=2)
@@ -28,6 +52,7 @@ class Encoder(nn.Module):
         output = self.bn2(self.conv2(self.lrelu(output1)))
         return output, encoder_dict
     
+# DECODER
 class Decoder(nn.Module):
     def __init__(self, input_dim=1, conv_dim=64):
         self.deconv1 = nn.ConvTranspose2d(conv_dim*10, conv_dim*8, kernel_size=3)
@@ -44,7 +69,8 @@ class Decoder(nn.Module):
         
         output = torch.tanh(output2)
         return output   
-    
+
+# DISCRIMINATOR
 class Discriminator(nn.Module):
     def __init__(self, category_num, img_dim=3, disc_dim=64):
         super(Discriminator, self).__init__()
@@ -68,13 +94,11 @@ class Discriminator(nn.Module):
         # image: generated or sample data images for the discriminator
         batch_size = images.shape[0]
         output = self.conv1(images)
-        
         output = self.conv2(output)
-        
         output = self.conv3(output)
-        
         output = self.conv4(output)
         
+        # resize the output to match the fc layer
         output = output.reshape(batch_size, -1)
         
         tf_loss_logit = self.fc1(output)
