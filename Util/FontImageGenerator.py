13bf5bd10e527e196ba6731c14103069d81a9ffc
HsLee-kaist <hs.lee@kaist.ac.kr>
2022-11-23 07:57:45 +0900

    image generation

--- Util/FontImageGenerator.py
+++ Util/FontImageGenerator.py
@@ -7,21 +7,36 @@ import os
 
 def single_font_image_saver(ch, size, save_path, font_path,n):
     adding=4
-    font = ImageFont.truetype(font_path, size = size+adding)
+    font = ImageFont.truetype(font_path, size = 90)
     x, y = font.getsize(ch)
     font_size = max(x,y)
 
 
-    img = Image.new('RGB', (size, size), (255, 255, 255)).convert('L')
+    img = Image.new('RGB', (size, size), (0, 0, 0)).convert('L')
     draw = ImageDraw.Draw(img)
-    draw.text(((size-x)/2,(size-y)/2), ch, font=font)
-    # draw.text((20,20),"A",font=font)
+    draw.text(((size-x)/2,(size-y)/2), ch,255, font=font)
+    #draw.text((10,10),ch,255,font=font)
+    
+    
+    
+    tmp = img.getbbox()
+    if(tmp !=None):
+        hs,he,ws,we = max(tmp[0]-5,0), min(tmp[2]+5,size), max(tmp[1]-5,0), min(tmp[3]+5,size)
+    else:
+        hs,he,ws,we=0,size, 0,size
 
     img = np.array(img)
+    img = img[ws:we,hs:he]
+    img = 255 -img
+    """
+    img = np.array(img)
     tmp = np.where(img!=255)
+    print(min(tmp[0]), max(tmp[0]))
+    print(min(tmp[1]), max(tmp[1]))
     ws,we,hs,he = max(min(tmp[0]-5),0), min(max(tmp[0]+5),size), max(min(tmp[1])-5,0), min(max(tmp[1]+5),size)
     img = img[ws:we,hs:he]
-
+    """
+    
     w_left = int((size-img.shape[0])/2)
     w_right = size-img.shape[0]-w_left
     h_left = int((size-img.shape[1])/2)
@@ -32,23 +47,24 @@ def single_font_image_saver(ch, size, save_path, font_path,n):
     
     np.save(save_path+'/'+str(n)+ch,img, allow_pickle=True)
 
-    # plt.figure(figsize=(1,2))
-    # plt.imshow(img,cmap='gray')
-    # plt.show()
+    #plt.figure(figsize=(1,2))
+    #plt.imshow(img,cmap='gray')
+    #plt.show()
     
 def font_saver(size):
-    font_file = open("./FontType")
-    letter_file = open("./LetterType")
+    font_file = open("./Util/FontType")
+    letter_file = open("./Util/LetterType")
     font_list = font_file.read().split('\n')
     letter_list = letter_file.read().split()
     
     for cur_font in font_list:
-        font_path = "./Font/" + cur_font+'.ttf'
-        save_path = "./GeneratedFontImage/"+cur_font
+        font_path = "./Util/Font/" + cur_font+'.ttf'
+        save_path = "./Util/GeneratedFontImage/"+cur_font
         os.makedirs(save_path, exist_ok = True)
         n = 0
+        print(cur_font)
         for ch in letter_list:       
             single_font_image_saver(ch, size, save_path, font_path, n)
             n+=1
     
-font_saver(100)
+font_saver(128)
--- train/train.py
+++ train/train.py
@@ -1,12 +1,12 @@
 import numpy as np
 import os, datetime, time, glob
-print(os.getcwd())
 import sys
-print(sys.path)
+cur_path = os.getcwd()
+sys.path.insert(0,cur_path)
 
 from Model.Backbone import BaseGenerator, Discriminator, BaseEncoder, SimpleDecoder, SimpleEncoder, BaseDecoder
 from Util.fontdataprovider import FontDataProvider
-from Util.Embedding import embedding_tensor_generate
+from Model.Embedding import embedding_tensor_generate
 from Util.FontLoader import train_loader, val_loader
 
 from tqdm import tqdm
@@ -14,32 +14,29 @@ from tqdm import tqdm
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision.utils import save_image
+
 
 import numpy as np
 import os, datetime, time, glob
 
 
 batch_size = 64
-
-def dataset():
-
-    return []
-
 def train():
     if torch.cuda.is_available():
         device = torch.device('cuda:0')
     else:
         device = torch.device('cpu')
-
-    epoch = 20
+    #device = torch.device('cpu')
+    epoch = 200
+    category_num = 25
 
     # Define the models
-    Gen = BaseGenerator() 
-    Enc = SimpleEncoder() 
-    Dec = BaseDecoder() 
-    Dis = Discriminator(category_num=50) 
+    Gen = BaseGenerator(category_num = category_num).to(device) 
+    Enc = BaseEncoder().to(device) 
+    Dis = Discriminator(category_num=category_num).to(device) 
 
     input_fonts = []
     source_dataset = []
@@ -54,40 +51,48 @@ def train():
     # L1 Loss, Constant Loss
     l1_criterion = nn.L1Loss(size_average=True)
     const_criterion = nn.MSELoss(size_average=True)
-    bce_criterion = nn.BCELoss(size_average=True)
+    bce_criterion = nn.BCEWithLogitsLoss(size_average=True)
     
-    category_num = 2
+    
     # Embedding
     embeddings = embedding_tensor_generate(category_num=category_num, embedding_dim=128)
+    #cur_path = os.getcwd()
+
+    print(cur_path)
+    model_path = os.path.join(cur_path,"results\\ckpt\\")
+    image_path = os.path.join(cur_path,"results\\fake-image\\")
     
-    model_path = "/ckpt/"
-    image_path = "/fake-image/"
+    
+    print(f"Model Path:{model_path}, Image Path:{image_path}")
     cur_iter = 0
+    image_size = 128
 
     font_data_provider = FontDataProvider(".")
-    train_dataloader = train_loader(batch_size=batch_size, font_data_provider=font_data_provider)
-    for cur_epoch in tqdm(range(epoch)):
+    #train_dataloader = train_loader(batch_size=batch_size, font_data_provider=font_data_provider)
 
+    for cur_epoch in tqdm(range(epoch)):
+    #for cur_epoch in range(epoch):
         # Penalties for generator's losses
         Lconst_penalty = 15
         L1_penalty = 100
         Lcategory_penalty = 1
 
         # Iterable dataset
-        
-        
+        train_dataloader = train_loader(batch_size=batch_size, font_data_provider=font_data_provider)
+
         # Forward
         for id, batch in enumerate(train_dataloader):
             cur_iter+=1
             ## x: input image
             ## real_image: real image 
+        
             x, real_image, font_nums = batch
-
+            x = x.detach().reshape(-1,1,image_size,image_size).to(device)
+            real_image = real_image.reshape(-1,1,image_size,image_size).to(device)
             # Init Dis params to zeros
-            Dis.zero_grad()
-
+            
             # Generate fake image from the generator
-            fake_image, encoded_source = Gen.forward(x, font_nums=font_nums, embedding=embeddings)
+            fake_image, encoded_source = Gen(x, font_nums=font_nums, embedding=embeddings)
             
             # Concatenate the input and the generated fake image, and get the discrimimator losses
             fake_patch = torch.cat([x, fake_image], dim=1)
@@ -100,20 +105,21 @@ def train():
             # real_loss = nn.BCELoss(real_result, 1)
             
             # Category Loss using the BCE Loss
-            real_category = "Something"
-
+            real_category = F.one_hot(torch.tensor(font_nums), category_num).to(device).to(torch.float)
+            #print(real_category.shape,len(font_nums) ,rcat_logit.shape)
             real_cat_loss = bce_criterion(rcat_logit, real_category)
             fake_cat_loss = bce_criterion(fcat_logit, real_category)
             cat_loss = Lcategory_penalty * 0.5 * (real_cat_loss + fake_cat_loss)
             
             # Labels
-            one_labels = torch.ones([batch_size,1]).to(device)
-            zero_labels = torch.zeros([batch_size,1]).to(device)
+            one_labels = torch.ones([x.shape[0],1]).to(device)
+            zero_labels = torch.zeros([x.shape[0],1]).to(device)
             
             # Binary Loss (T/F Loss from the Discriminator)
             real_bin_loss = bce_criterion(r_loss_logit, one_labels)
             fake_bin_loss = bce_criterion(f_loss_logit, zero_labels)
             bin_loss = fake_bin_loss + real_bin_loss
+            
             
             # L1 Loss of Fake and Real Madrid
             l1_loss = L1_penalty * l1_criterion(real_image, fake_image)
@@ -122,35 +128,45 @@ def train():
             encoded_fake = Enc(fake_image)[0]
             const_loss = Lconst_penalty * const_criterion(encoded_source, encoded_fake)
             
+            # Cheat Loss for fool model's discriminator
+            cheat_loss = bce_criterion(f_loss_logit, one_labels)
+        
+           
+            D_loss = cat_loss + bin_loss
+            G_loss = fake_cat_loss + const_loss + l1_loss + cheat_loss
+            
             # Gradient Update
             # Discriminator Backward Process
-            D_loss = cat_loss + bin_loss
             Dis.zero_grad()
-            D_loss.backward()
+            D_loss.backward(retain_graph=True)
             D_optimizer.step()
             
+            
             # Generator Backward Process
-            G_loss = fake_bin_loss + const_loss + l1_loss
+            
             Gen.zero_grad()
-            G_loss.backward()
+            G_loss.backward(retain_graph=True)
             G_optimizer.step()
             
             # Log
-            if (id + 1) % log_step == 0:
-                log_string = f"Epoch[{cur_epoch}/{epoch}], step[{id}/{batch_size}], \
-                    l1_loss: {l1_loss.item()}, d_loss: {D_loss.item()}, g_loss: {G_loss.item()}"
+            #if (id + 1) % log_step == 0:
+            if (id) % 10 == 0:
+                log_string = f"Epoch[{cur_epoch}/{epoch}], step[{id}], l1_loss: {l1_loss.item()}, d_loss: {D_loss.item()}, g_loss: {G_loss.item()}"
                 print(log_string)
 
             # Save Images
-            if (id + 1) % 350 == 0:
+            #if (id + 1) % 350 == 0:
+            if (id) % 20== 0:
                 id_save_path = os.path.join(image_path, "fake-img-%d-%d.png" % (epoch, id+1))
+                id_save_path_t = os.path.join(image_path, "true-img-%d-%d.png" % (epoch, id+1))
                 save_image(fake_image.data, id_save_path, nrow=8, pad_value=255)
-
+                save_image(real_image.data, id_save_path_t, nrow=8, pad_value=255)
+    
         # Save Checkpoints
         now = datetime.datetime.now()
-        now_date = now.strftime("%m%d")
+        now_date = now.strftime("%m-%d")
         now_time = now.strftime('%H:%M')
-        torch.save(Gen.state_dict(), os.path.join(model_path, f"{epoch}/{now_date}/{now_time}Gen.ckpt"))
-        torch.save(Dis.state_dict(), os.path.join(model_path, f"{epoch}/{now_date}/{now_time}Dis.ckpt"))
+        torch.save(Gen.state_dict(), os.path.join(model_path, f"Gen{epoch}-{now_date}-{now_time}.ckpt"))
+        torch.save(Dis.state_dict(), os.path.join(model_path, f"Dis{epoch}-{now_date}-{now_time}.ckpt"))
             
 train()
